ea1da2014cfc2d50c2296eb7e029ae712a56bef9
macken-hem <ken.kgaf@gmail.com>
2019-12-12 15:04:29 +0900

    up

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
+<template>
+  <div>
+    <LikeHeader></LikeHeader>
+    <LikeNumber></LikeNumber>
+  </div>
+</template>
 
 <script>
+import 'normalize.css'
+import LikeHeader from "./components/LikeHeader.vue"
 
+export default {
+  components: {
+    LikeHeader
+  }
+};
 </script>
 
-<style>
-
+<style scoped>
+div {
+  border: 1px solid blue;
+}
 </style>
